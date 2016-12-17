37474a02cbdf0c860a182cb260cddf45888a7096
linguanghua <962792501@qq.com>
2016-12-17 18:37:44 +0800

    Struts2新添加登录功能

--- src/com/junxu/action/LoginAction.java
+++ src/com/junxu/action/LoginAction.java
@@ -1,13 +1,22 @@
 package com.junxu.action;
 
+import com.junxu.po.User;
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
 
 /**
  * Created by LinXu on 2016/12/17.
  */
-public class LoginAction extends ActionSupport{
+public class LoginAction extends ActionSupport implements ModelDriven<User>{
+    User user = new User();
     @Override
     public String execute() throws Exception {
+        System.out.println(user.getUsername());
         return SUCCESS;
+    }
+
+    @Override
+    public User getModel() {
+        return user;
     }
 }
---
+++ src/com/junxu/po/User.java
@@ -0,0 +1,25 @@
+package com.junxu.po;
+
+/**
+ * Created by LinXu on 2016/12/17.
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
