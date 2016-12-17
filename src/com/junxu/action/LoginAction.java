bb2397993dd9dd0c1b20808a843b1ace1a648621
linguanghua <962792501@qq.com>
2016-12-17 13:53:35 +0800

    创建了一个应用

---
+++ src/com/junxu/action/LoginAction.java
@@ -0,0 +1,13 @@
+package com.junxu.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by LinXu on 2016/12/17.
+ */
+public class LoginAction extends ActionSupport{
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+}
